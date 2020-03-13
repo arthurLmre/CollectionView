4bc8d2687d36b0ce10fba9a926dc2efe1fe4bd3e
lpiem <arthurlamure01@gmail.com>
2020-03-13 11:23:55 +0100

    Init Commit

---
+++ CollectionViewTest/CollectionViewTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewTest
+//
+//  Created by lpiem on 13/03/2020.
+//  Copyright © 2020 lpiem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
